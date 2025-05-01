df856343862858eeadcc6f01cfc50e01ecec28a3
thew0lf <bill@tandgconsulting.com>
2025-04-30 20:51:26 -0400

    update eslint.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './resources/**/*.tsx',
+        './resources/**/*.jsx',
+        './resources/**/*.js',
+        './resources/**/*.php',
+        './resources/**/*.html',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
