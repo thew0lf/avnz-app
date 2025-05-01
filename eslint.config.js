44b3ecd2ae83d21cb0f5655f88208556b66638be
thew0lf <bill@tandgconsulting.com>
2025-04-30 20:22:55 -0400

    update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,45 @@
 import { defineConfig } from 'eslint-define-config';
 
 export default defineConfig({
+    env: {
+        browser: true,
+        node: true,
+        es2021: true,
+    },
     parser: '@typescript-eslint/parser',
     plugins: [
         '@typescript-eslint',
         'react',
         'react-hooks',
-        'jsx-a11y'
+        'jsx-a11y',
     ],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
         'plugin:react-hooks/recommended',
-        'plugin:jsx-a11y/recommended'
+        'plugin:jsx-a11y/recommended',
     ],
     parserOptions: {
         ecmaVersion: 'latest',
         sourceType: 'module',
         ecmaFeatures: {
-            jsx: true
-        }
+            jsx: true,
+        },
     },
     settings: {
         react: {
-            version: 'detect'
-        }
+            version: 'detect',
+        },
     },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            parser: '@typescript-eslint/parser',
+            extends: ['plugin:@typescript-eslint/recommended'],
+        },
+    ],
     rules: {
-        // TODO: add or override project-specific rules here
-    }
+        // Add or override project-specific rules here
+    },
 });
